75788725e3a3a7c9896f55e7ae9b93677663c49c
Farraï Sofiane <sofiane.farrai@efrei.net>
2025-12-19 11:15:51 +0100

    après avoir créer Contact, on redirige la demande du produit dans le formulaire de contact.

--- TPFinal/src/Controller/ContactController.php
+++ TPFinal/src/Controller/ContactController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $this->addFlash('success', 'Merci ! Votre message a été envoyé avec succès.');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
         ]);
     }
 }
